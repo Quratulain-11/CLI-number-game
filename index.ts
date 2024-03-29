02e826a1493df47fe8c7ba6f66c2c2438574b154
Qurat ul Ain <160419588+Qurat-01@users.noreply.github.com>
2024-03-29 09:15:11 -0700

    cli_number_guessing_game

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+const random_number = Math.floor(Math.random() * 10 + 1);
+
+
+const answers = await inquirer.prompt( 
+[
+ {
+   name : "userguessednumber",
+   type : "number",
+   message : "please guess a number between (1-10)"
+},
+]
+);
+
+if (answers.userguessednumber === random_number)
+{
+console.log("congratulations! you guess a right number..")
+}
+
+else
+{
+console.log("sorry! you guessed wrong number..")
+};
